0031fdd2e3200f0587b0d46f8eb02a5db644af46
kal1van1ch <mishadolgopolov668@gmail.com>
2026-06-11 23:34:57 +0300

    Добавил обработку CORS запроса

--- src/main/java/com/kal1van1ch/wordrop/controller/UserAuthController.java
+++ src/main/java/com/kal1van1ch/wordrop/controller/UserAuthController.java
@@ -8,15 +8,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin(origins = "http://localhost:5173")
 public class UserAuthController {
     private static final Logger log = LoggerFactory.getLogger(UserAuthController.class);
     private final UserService userService;
